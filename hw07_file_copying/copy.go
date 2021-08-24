9ef0cdcb3b68e420aea6c0ef953a97c6b193c1ae
lubitel-aka-graber05 <dronihash@gmail.com>
2021-08-24 23:12:57 +0300

    HW07 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,74 @@ package main
 
 import (
 	"errors"
+	"github.com/cheggaaa/pb/v3"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrIncorrectOffsetValue  = errors.New("offset value must not be negative")
+	tmpl                     = `{{ counters . | green}} {{ bar . "<" "-" 
+								(cycle . "↖" "↗" "↘" "↙" ) "." ">" | red}} {{percent . | green}}`
 )
 
+func closeFile(file *os.File) {
+	if err := file.Close(); err != nil {
+		log.Fatal(err)
+	}
+}
+
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	file, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer closeFile(file)
+
+	fileStat, err := file.Stat()
+	if err != nil {
+		return err
+	}
+	if !fileStat.Mode().IsRegular() {
+		return ErrUnsupportedFile
+	}
+	if offset > fileStat.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+	if offset < 0 {
+		return ErrIncorrectOffsetValue
+	}
+	if limit > fileStat.Size()-offset {
+		limit = fileStat.Size() - offset
+	}
+
+	switch {
+	case offset != 0 || limit != 0:
+		editFile := io.NewSectionReader(file, offset, limit)
+		buf := make([]byte, editFile.Size())
+		bar := pb.ProgressBarTemplate(tmpl).Start64(editFile.Size())
+		defer bar.Finish()
+		if _, err := bar.NewProxyReader(editFile).Read(buf); err != nil && err != io.EOF {
+			return err
+		}
+		if err := ioutil.WriteFile(to, buf, 0644); err != nil {
+			return err
+		}
+	case offset == 0 && limit == 0:
+		bar := pb.ProgressBarTemplate(tmpl).Start64(fileStat.Size())
+		defer bar.Finish()
+		buf := make([]byte, fileStat.Size())
+		if _, err := bar.NewProxyReader(file).Read(buf); err != nil && err != io.EOF {
+			return err
+		}
+		if err = ioutil.WriteFile(to, buf, 0644); err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,33 @@
 package main
 
-import "testing"
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("test for unsupported file", func(t *testing.T) {
+		err := Copy("/dev/urandom", "/tmp/output.txt", 0, 0)
+		require.ErrorIs(t, err, ErrUnsupportedFile, "unsupported files")
+	})
+
+	t.Run("negative offset", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "/tmp/output.txt", -1, 0)
+		require.ErrorIs(t, err, ErrIncorrectOffsetValue, "incorrect offset")
+	})
+
+	t.Run("offset > file sizes", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "/tmp/output.txt", 7000, 0)
+		require.ErrorIs(t, err, ErrOffsetExceedsFileSize, "Offset is larger than the file size")
+	})
+
+	t.Run("offset = 0 & limit = 0", func(t *testing.T) {
+		err := Copy("input.txt", "/tmp/output.txt", 0, 0)
+		require.NoError(t, err)
+	})
+
+	t.Run("offset = 1 & limit = 10", func(t *testing.T) {
+		err := Copy("input.txt", "/tmp/output.txt", 1, 10)
+		require.NoError(t, err)
+	})
 }
